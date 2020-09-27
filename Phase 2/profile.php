0f07d64e591a2dab78e1d83b7b808079c89a5b1c
MT-Cobbler <mtschoeman123@gmail.com>
2020-09-27 18:39:11 +0200

    a

--- Phase 2/profile.php
+++ Phase 2/profile.php
@@ -1,13 +1,19 @@
 <?php
 	
-	$server = "localhost";
-	$username = "u17029377";
-	$password = "mzjxkpgl";
-	$database = "dbu17029377";
-	$mysqli = mysqli_connect($server, $username, $password, $database);
+//	$server = "localhost";
+//	$username = "u17029377";
+//	$password = "mzjxkpgl";
+//	$database = "dbu17029377";
+//	$mysqli = mysqli_connect($server, $username, $password, $database);
 
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "dbu17029377";
+$mysqli = mysqli_connect($server, $username, $password, $database);
 	
 $email = isset($_GET['user_Id']) ? $_GET['user_Id'] : false;
+$pass = isset($_GET['user_P']) ? $_GET['user_P'] : false;
 
 if (isset($_POST['submit'])) {
 	
@@ -83,12 +89,13 @@ if(isset($_POST['upload'])){
 ?>
 <!DOCTYPE html>
 <html>
+
 <head>
-	<title>P. Exhibit - Profile</title>
-	<meta charset="utf-8">
-	<meta name="author" content="Matthew Schoeman">
-	<meta name="viewport" content="width=device-width,initial-1">
-	<!--Bootstrap CDN-->
+    <title>P. Exhibit - Profile</title>
+    <meta charset="utf-8">
+    <meta name="author" content="Matthew Schoeman">
+    <meta name="viewport" content="width=device-width,initial-1">
+    <!--Bootstrap CDN-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
@@ -96,30 +103,31 @@ if(isset($_POST['upload'])){
     <!--Font Awesome CDN-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous">
     <!--style.css-->
-    
+
     <link rel="apple-touch-icon" sizes="57x57" href="media/favicon/apple-icon-57x57.png">
-	<link rel="apple-touch-icon" sizes="60x60" href="media/favicon/apple-icon-60x60.png">
-	<link rel="apple-touch-icon" sizes="72x72" href="media/favicon/apple-icon-72x72.png">
-	<link rel="apple-touch-icon" sizes="76x76" href="media/favicon/apple-icon-76x76.png">
-	<link rel="apple-touch-icon" sizes="114x114" href="media/favicon/apple-icon-114x114.png">
-	<link rel="apple-touch-icon" sizes="120x120" href="media/favicon/apple-icon-120x120.png">
-	<link rel="apple-touch-icon" sizes="144x144" href="media/favicon/apple-icon-144x144.png">
-	<link rel="apple-touch-icon" sizes="152x152" href="media/favicon/apple-icon-152x152.png">
-	<link rel="apple-touch-icon" sizes="180x180" href="media/favicon/apple-icon-180x180.png">
-	<link rel="icon" type="image/png" sizes="192x192"  href="media/favicon/android-icon-192x192.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="media/favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="96x96" href="media/favicon/favicon-96x96.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="media/favicon/favicon-16x16.png">
-	<link rel="manifest" href="/manifest.json">
-	<meta name="msapplication-TileColor" content="#000000">
-	<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
-	<meta name="theme-color" content="#000000">
-	<link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="apple-touch-icon" sizes="60x60" href="media/favicon/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="media/favicon/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="media/favicon/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="media/favicon/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="media/favicon/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="media/favicon/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="media/favicon/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="media/favicon/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="media/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="media/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="media/favicon/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="media/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/manifest.json">
+    <meta name="msapplication-TileColor" content="#000000">
+    <meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+    <meta name="theme-color" content="#000000">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="profilePic.css">
 </head>
 
 <body id="homeContainer">
-	
-	<?php
+
+    <?php
 
 		if($email){
 			$getID = "SELECT email FROM usertable WHERE email = '$email'";
@@ -137,50 +145,92 @@ if(isset($_POST['upload'])){
 								<span> <img src="media/logo6.png" width="80" height="80" alt="logo"> </span>
 								<span class="display-4"> Hello: '.$row['username'].'</span>
 							</div>
-							<div class="col-12 col-sm-12 col-md-6">
-									
+							<div class="col-12 col-sm-12 col-md-6">	
 								<li><a href="#about">About</a></li>
 								<li><a href="#contact">Contact</a></li>
 								<li><a href="profile.php">Profile</a></li>
 							  	<li><a class="active" href="home.php">Home</a></li>
 							</div>
-							
 						</ul>
-						
 					</nav>
-
-					<div class="container-fluid">
-							<div class="row">
-								<div class="col-md-3 col-12" style="background-color:#944170">
-									<div class="card" >
-										<form action="profile.php" method="GET" enctype="multipart/form-data" class="container" id="imageUpload" style="background-color:#944170; border:none;">
-												<div class="card-header">
-													Profile Photo
-												</div>
-												<div>';
-												$query = "SELECT * FROM userimages  WHERE email = '$userID' ORDER BY date DESC";
-												$result = $mysqli->query($query);
-												$pshow = $result->fetch_assoc();
-												if($pshow['profilepic']){
-												echo'	<img class="card-img" src="gallery/'.$pshow['profilepic'].'">';
-												}
-												else{
-													echo "No profile picture";
-												}
-												echo'</div>
-												<label>Change your profile photo</label>
-												<input type="file" name="profile"/>
-												<input type="text" name="user_Id" value="'.$email.'"/>
-												<button type="submit" class="btn btn-light" name="upload">Upload</button>
-										</form>
-									</div>
-								</div>
 					';	
 				}	
-		}	
-		
+		}		
 	?>
-	
+
+    <div class="container-fluid">
+        <div class="container" id="topBar">
+            <div class="row">
+                <div class="col-4" id="topBar_Left">
+                    <img src="profilePics/logo6.png" alt="profilePic">
+                </div>
+                <div class="col-8" id="topBar_Right">
+                    <div class="row">
+                        <div class="col-6"> Username</div>
+                        <div class="col">
+                            <form method="POST" action="editprofile.php">
+                                <input type="hidden" value="matsch@gmail.com" name="email">
+                                <input type="hidden" value="Pancakes#1" name="userPass">
+                                <button type="submit" class="btn btn-light">Edit Profile</button>
+
+                            </form>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-4">
+                            <h5>posts</h5>
+                        </div>
+                        <div class="col-4">
+                            <h5>Followers</h5>
+                        </div>
+                        <div class="col-4">
+                            <h5>Following</h5>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <h5><b>name</b></h5>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-6 border">
+                    <!--Posts-->
+                    <div class="card border">
+
+                        <div class="card-title">hello
+                        </div>
+                        <div class="card-body">
+                            <div class="card-img">
+                                <img src="profilePics/logo6.png">
+                            </div>
+                            <div class="card-text">
+                                fill em with the vennon and eliminate them
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-6 border">
+                    <!--Albums-->
+                    <div class="card border">
+
+                        <div class="card-title">hello
+                        </div>
+                        <div class="card-body">
+                            <div class="card-img">
+                                <img src="profilePics/logo6.png">
+                            </div>
+                            <div class="card-text">
+                                fill em with the vennon and eliminate them
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </body>
 
 </html>
